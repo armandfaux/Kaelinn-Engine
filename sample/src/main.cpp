ed0d0c7a06c709bfbcb213570c08f7e4f36b7a3e
armandfaux <armand.faux@epitech.eu>
2024-03-08 17:39:22 +0100

    [ADD] Sample project

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -3,15 +3,37 @@
 #include <engine/system/input/InputSystem.hpp>
 #include <engine/system/physic/PhysicSystem.hpp>
 #include <engine/system/scene/SceneSystem.hpp>
+#include <engine/component/spatial/SpatialComp.hpp>
+#include <engine/component/sprite/SpriteComp.hpp>
 
 int main(int argc, char** argv) {
 
+    // Core initialisation
     kln::Game game;
 
-    std::shared_ptr<kln::RenderSystem> renderS = std::make_shared<kln::RenderSystem>("Simulation", 1000, 800, 32, "Fullscreen");
+    // Systems initialisation
+    std::shared_ptr<kln::RenderSystem> renderS = std::make_shared<kln::RenderSystem>("Simulation", 1260, 720, 32, "Default");
+    std::shared_ptr<kln::InputSystem> inputS = std::make_shared<kln::InputSystem>(renderS->getWindow());
     std::shared_ptr<kln::PhysicSystem> physicS = std::make_shared<kln::PhysicSystem>();
+    std::shared_ptr<kln::SceneSystem> sceneS = std::make_shared<kln::SceneSystem>(game);
 
-    game.createScene("Main Scene");
+    game.addSystem(sceneS);
+    game.addSystem(inputS);
+    game.addSystem(physicS);
+    game.addSystem(renderS);
+
+    // Scene creation
+    game.createScene("Main");
+    game.setActiveScene("Main");
+    std::shared_ptr<kln::Scene> mainScene = game.getSceneByName("Main");
+
+    // Create a simple entity
+    std::shared_ptr<kln::Entity> ball = mainScene->createEntity("Ball");
+    ball->addComp(std::make_shared<kln::SpatialComp>(kln::Vector2(600, 320)));
+    ball->addComp(std::make_shared<kln::SpriteComp>("assets/ball.png", kln::Vector2(80, 80)));
+
+    // Starting game loop
+    game.run();
 
     return 0;
 }
